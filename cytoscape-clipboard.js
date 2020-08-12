351368b9ce75c7bd3fed7344578161f9da8cd0c6
Nikolai Nøjgaard <nojgaard@imada.sdu.dk>
2020-08-12 17:22:47 +0200

    fixed clipboard

--- cytoscape-clipboard.js
+++ cytoscape-clipboard.js
@@ -1,4 +1,4 @@
-;(function () {
+; (function () {
     'use strict';
 
     // registers the extension on a cytoscape lib ref
@@ -15,16 +15,17 @@
 
             //Global variables to hold x and y coordinates in case of pasting
             var mouseX, mouseY;
-            cy.on('mousemove', function onmousemove (e) {
-                var pos = e.position || e.cyPosition;
-                mouseX = pos.x;
-                mouseY = pos.y;
-            });
+            // cy.on('mousemove', function onmousemove(e) {
+            //     var pos = e.position || e.cyPosition;
+            //     mouseX = pos.x;
+            //     mouseY = pos.y;
+            // });
 
 
             var options = {
                 beforeCopy: null,
                 afterCopy: null,
+                oldIdToNewId: null,
                 beforePaste: null,
                 afterPaste: null
             };
@@ -34,7 +35,7 @@
 
             function getScratch() {
                 if (!cy.scratch("_clipboard")) {
-                    cy.scratch("_clipboard", { });
+                    cy.scratch("_clipboard", {});
 
                 }
                 return cy.scratch("_clipboard");
@@ -66,16 +67,22 @@
             }
 
 
-            var oldIdToNewId = {};
+            var oldIdToNewId = new Map();
 
             function changeIds(jsons, pasteAtMouseLoc) {
                 jsons = $.extend(true, [], jsons);
+
                 for (var i = 0; i < jsons.length; i++) {
                     var jsonFirst = jsons[i];
-                    var id = getCloneId();
-                    oldIdToNewId[jsonFirst.data.id] = id;
+                    if (oldIdToNewId.has(jsonFirst.data.id)) {
+                        var id = oldIdToNewId.get(jsonFirst.data.id);
+                    } else {
+                        var id = getCloneId();
+                        oldIdToNewId.set(jsonFirst.data.id, id);
+                    }                  
                     jsonFirst.data.id = id;
                 }
+
 
                 //Paste the elements centered at the mouse location
                 var topLeftX, topLeftY;
@@ -83,8 +90,7 @@
                 var centerX, centerY;
                 var diffX, diffY;
                 //Checks only for nodes
-                if (jsons[0] !== undefined && jsons[0].position.x)
-                {
+                if (jsons[0] !== undefined && jsons[0].position !== undefined) {
                     topLeftX = jsons[0].position.x;
                     topLeftY = jsons[0].position.y;
                     bottomRightX = jsons[0].position.x;
@@ -106,11 +112,11 @@
                             bottomRightY = ele.position.y;
                         }
                     }
-                    centerX = (topLeftX+bottomRightX)/2;
-                    centerY = (topLeftY+bottomRightY)/2;
+                    centerX = (topLeftX + bottomRightX) / 2;
+                    centerY = (topLeftY + bottomRightY) / 2;
 
-                    diffX = mouseX -centerX;
-                    diffY = mouseY - centerY;
+                    diffX = cy.mouseX - centerX;
+                    diffY = cy.mouseY - centerY;
                 }
 
                 for (var j = 0; j < jsons.length; j++) {
@@ -118,11 +124,11 @@
                     var fields = ["source", "target", "parent"];
                     for (var k = 0; k < fields.length; k++) {
                         var field = fields[k];
-                        if (json.data[field] && oldIdToNewId[json.data[field]])
-                            json.data[field] = oldIdToNewId[json.data[field]];
+                        if (json.data[field] && oldIdToNewId.get(json.data[field]))
+                            json.data[field] = oldIdToNewId.get(json.data[field]);
 
                     }
-                    if (json.position.x) {
+                    if (json.position !== undefined) {
                         if (pasteAtMouseLoc == false) {
                             json.position.x += 50;
                             json.position.y += 50;
@@ -147,16 +153,16 @@
                 scratchPad.instance = {
                     copy: function (eles, _id) {
                         var id = _id ? _id : getItemId();
-                        eles.unselect();
+                        //eles.unselect();
                         var descs = eles.nodes().descendants();
-                        var nodes = eles.nodes().union(descs).filter(":visible");
-                        var edges = nodes.edgesWith(nodes).filter(":visible");
+                        var nodes = eles.nodes().union(descs).filter(":selectable");
+                        var edges = nodes.edgesWith(nodes).filter(":selectable");
 
-                        if(options.beforeCopy) {
+                        if (options.beforeCopy) {
                             options.beforeCopy(nodes.union(edges));
                         }
-                        clipboard[id] = {nodes: nodes.jsons(), edges: edges.jsons()};
-                        if(options.afterCopy) {
+                        clipboard[id] = { nodes: nodes.jsons(), edges: edges.jsons() };
+                        if (options.afterCopy) {
                             options.afterCopy(clipboard[id]);
                         }
                         return id;
@@ -164,10 +170,13 @@
                     paste: function (_id, pasteAtMouseLoc) {
                         var id = _id ? _id : getItemId(true);
                         var res = cy.collection();
-                        if(options.beforePaste) {
+                        if (options.beforePaste) {
                             options.beforePaste(clipboard[id]);
                         }
                         if (clipboard[id]) {
+                            if (options.oldIdToNewId) {
+                                oldIdToNewId = options.oldIdToNewId(clipboard[id]);
+                            }
                             var nodes = changeIds(clipboard[id].nodes, pasteAtMouseLoc);
                             var edges = changeIds(clipboard[id].edges);
                             oldIdToNewId = {};
@@ -177,7 +186,7 @@
                             });
 
                         }
-                        if(options.afterPaste) {
+                        if (options.afterPaste) {
                             options.afterPaste(res);
                         }
                         cy.trigger('pasteClonedElements');
